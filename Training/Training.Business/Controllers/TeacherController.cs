67382705d882b1aca78427f93c44dd91376a08dd
Lavriona <alena10586@gmail.com>
2018-10-20 12:08:26 +0300

    adding Training

---
+++ Training/Training.Business/Controllers/TeacherController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Training.Business.Models;
+using Training.Business.Helpers;
+
+namespace Training.Business.Controllers
+{
+    public class TeacherController
+    {
+        /// <summary>
+        /// Create Teacher in current group
+        /// </summary>
+        /// <param name="group">Current group</param>
+        /// <returns></returns>
+        public static Teacher Create(Group group)
+        {
+            Console.WriteLine("\r\nВведите ФИО преподователя: ");
+
+            Teacher teacher = new Teacher();
+
+            teacher.Fullname = Console.ReadLine();
+
+            teacher.group = group;
+
+            while (true)
+            {
+                Console.WriteLine("\r\nВведите тип преподавателя: ");
+
+                int j = 0;
+                foreach (var type in Enum.GetNames(typeof(Teacher.EnumTypes)))
+                {
+                    j++;
+                    Console.WriteLine($"{j} - {type}");
+                }
+
+                string inputTeacherType = Console.ReadLine();
+
+                if (byte.TryParse(inputTeacherType, out byte teacherType) && Enum.GetName(typeof(Teacher.EnumTypes), --teacherType) != null)
+                {
+                    teacher.Type = teacherType.ToString();
+
+                    break;
+                }
+                else
+                {
+                    Helper.ShowMessage("Такого типа нет в системе!");
+                }
+            }
+
+            return teacher;
+        }
+
+        /// <summary>
+        /// Show teachers
+        /// </summary>
+        /// <param name="teachers">Teachers for show</param>
+        public static void Show(Teacher[] teachers)
+        {
+            for (int i = 0; i < teachers.Length; i++)
+            {
+                Console.WriteLine($"{i + 1} - {teachers[i].Fullname} ({teachers[i].Type}, {teachers[i].group.Name})");
+            }
+        }
+    }
+}
---
+++ Training/Training/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Training.Business.Models;
+using Training.Business.Controllers;
+using Training.Business.Helpers;
+
+namespace Training
+{
+    class Program
+    {
+        static Group[] groups = new Group[0];
+        static Teacher[] teachers = new Teacher[0];
+        static Student[] students = new Student[0];
+
+        static void Main(string[] args)
+        {
+            int groupCount = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Введите кол-во групп: ");
+
+                string inputGroupCount = Console.ReadLine();
+
+                if (int.TryParse(inputGroupCount, out groupCount))
+                {
+                    break;
+
+                }
+                else
+                {
+                    Helper.ShowMessage("Введенное значениe не является числом!");
+                }
+            }
+
+            Array.Resize(ref teachers, teachers.Length + groupCount);
+
+            Array.Resize(ref groups, groups.Length + groupCount);
+
+            for (int i = 0; i < groups.Length; i++)
+            {
+                Console.Clear();
+
+                Console.WriteLine($"Группа #{i + 1} \r\n");
+
+                groups[i] = GroupController.Create();
+
+                teachers[i] = TeacherController.Create(groups[i]);
+
+                groups[i].Teacher = teachers[i];
+
+                while (true)
+                {
+                    Console.WriteLine($"\r\nВведите кол-во студенов (не больше {groups[i].CountStudents}):");
+
+                    string inputStudentsCount = Console.ReadLine();
+
+                    if (byte.TryParse(inputStudentsCount, out byte studentsCount))
+                    {
+                        if (studentsCount <= groups[i].CountStudents)
+                        {
+                            groups[i].students = new Student[studentsCount];
+
+                            for (int j = 0; j < studentsCount; j++)
+                            {
+                                groups[i].students[j] = StudentController.Create(groups[i]);
+
+                                Array.Resize(ref students, students.Length + 1);
+
+                                students[students.Length - 1] = groups[i].students[j];
+                            }
+                            break;
+                        }
+                        else
+                        {
+                            Helper.ShowMessage("Введенное значение превышает допустимое кол-во! Распределите в другие группы.");
+                        }
+                    }
+                    else
+                    {
+                        Helper.ShowMessage("Введенное значение на является числом!");
+                    }
+                }
+
+            }
+
+            GetOptions();
+
+            Console.ReadKey();
+        }
+
+        public static void GetOptions()
+        {
+            while (true)
+            {
+                Console.WriteLine("\r\nВведите номер операции:");
+                Console.WriteLine("1 - Вывести список студентов");
+                Console.WriteLine("2 - Вывести список преподавателей");
+                Console.WriteLine("3 - Вывести информацтю по группам");
+
+                string inputOption = Console.ReadLine();
+
+                if (int.TryParse(inputOption, out int option))
+                {
+                    Console.Clear();
+                    switch (option)
+                    {
+                        case 1:
+                            Helper.ShowMessage("\r\nСписок студентов: ", ConsoleColor.Green);
+                            StudentController.Show(students);
+                            break;
+                        case 2:
+                            Helper.ShowMessage("\r\nСписок преподавателей: ", ConsoleColor.Green);
+                            TeacherController.Show(teachers);
+                            break;
+                        case 3:
+                            Helper.ShowMessage("\r\nИнформация по группам: ", ConsoleColor.Green);
+                            GroupController.Show(groups);
+                            break;
+                        default:
+                            Helper.ShowMessage("Номер введенной операции не доступен!");
+                            break;
+                    }
+                }
+                else
+                {
+                    Helper.ShowMessage("Введенное значение не является числом!");
+                }
+            }
+        }
+    }
+}
